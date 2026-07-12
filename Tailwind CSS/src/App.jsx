ee1f31ae07bfc63eacbbedcc7dcc3fa7cd630a73
smitrabari <smitrabari18@gmail.com>
2026-07-12 21:08:38 +0530

    Page for Windows Users

--- Tailwind CSS/src/App.jsx
+++ Tailwind CSS/src/App.jsx
@@ -6,7 +6,8 @@ function App() {
   return (
     <>
       <div className="bg-yellow-100 min-h-screen px-6 py-3 flex flex-col gap-16
-      md:px-10 md:py-7 justify-between">
+      md:px-10 md:py-7 justify-between
+      lg:px-20 lg:py-10">
         <Header />
         <Body />
         <Footer />
