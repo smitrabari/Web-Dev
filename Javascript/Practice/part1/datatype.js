1f49b309254356f7894d97c13eb1ccd47ae8cf0c
smitrabari <smitrabari18@gmail.com>
2026-08-23 19:16:02 +0530

    Undefined as Datatype

--- Javascript/Practice/part1/datatype.js
+++ Javascript/Practice/part1/datatype.js
@@ -4,5 +4,8 @@ console.log(typeof age); //represent the datatype
 let name = "Smit"; //string
 console.log(typeof name); //represent the datatype
 
-let isstudent = true; //string
+let isstudent = true; //Boolean
 console.log(typeof isstudent); //represent the datatype
+
+let city; //Undefined
+console.log(typeof city); //represent the datatype
