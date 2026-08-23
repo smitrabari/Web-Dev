93bbdb597be8c1d83aa72751570674879c7daf73
smitrabari <smitrabari18@gmail.com>
2026-08-23 19:14:46 +0530

    Boolean as Datatype

--- Javascript/Practice/part1/datatype.js
+++ Javascript/Practice/part1/datatype.js
@@ -3,3 +3,6 @@ console.log(typeof age); //represent the datatype
 
 let name = "Smit"; //string
 console.log(typeof name); //represent the datatype
+
+let isstudent = true; //string
+console.log(typeof isstudent); //represent the datatype
