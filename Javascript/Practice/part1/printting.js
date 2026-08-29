7fb67a4502b1d646c175dd808233d1278134e000
smitrabari <smitrabari18@gmail.com>
2026-08-29 15:47:38 +0530

    Javascript Revision

---
+++ Javascript/Practice/part1/printting.js
@@ -0,0 +1,5 @@
+console.log("hello!!"); //print in one line
+console.log("world!!"); // brings to next line
+process.stdout.write("kem choo!!"); //prints every thing in one line
+process.stdout.write("kem choo!!");
+process.stdout.write("kem choo!!");
