f03718193e67fb532dfdff283918ae7adf7bd513
smitrabari <smitrabari18@gmail.com>
2026-08-29 16:36:33 +0530

    Javascript Revision

---
+++ Javascript/Practice/part1/changes.js
@@ -0,0 +1,8 @@
+// changing vaibale after instialization
+let gameName = "Grand Theft Auto V";
+gameName = "Grand Theft Auto VI";
+console.log(gameName);
+
+// constant variable
+const ageLimit = "18+";
+console.log(`Age limit for ${gameName} is ${ageLimit}`);
