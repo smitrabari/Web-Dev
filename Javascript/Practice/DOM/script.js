8cf3bce314836f497b0a194feab59b4b601eb934
smitrabari <smitrabari18@gmail.com>
2026-09-06 14:58:08 +0530

    Exercise 1 of DOM Manipulation

---
+++ Javascript/Practice/DOM/script.js
@@ -0,0 +1,5 @@
+// exercise 1
+document.getElementById("button1").addEventListener("click", function () {
+  let paragraphchange = document.getElementById("paragraph1");
+  paragraphchange.textContent = "Text changed!";
+});
