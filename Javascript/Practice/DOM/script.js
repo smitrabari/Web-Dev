fd52769fe0153f4c823881a9f53d42f423c7cefa
smitrabari <smitrabari18@gmail.com>
2026-09-06 15:45:19 +0530

    Exercise 2 of DOM Manipulation

--- Javascript/Practice/DOM/script.js
+++ Javascript/Practice/DOM/script.js
@@ -3,3 +3,9 @@ document.getElementById("button1").addEventListener("click", function () {
   let paragraphchange = document.getElementById("paragraph1");
   paragraphchange.textContent = "Text changed!";
 });
+
+// exercise 2
+document.getElementById("button2").addEventListener("click", function () {
+  let cities = document.getElementById("list1");
+  cities.firstElementChild.classList.add("highlight");
+});
