9ee3770824e7bae71f0276c86c61ed2cccc114d3
smitrabari <smitrabari18@gmail.com>
2026-09-07 22:40:09 +0530

    Aync Await

--- Javascript/Practice/Javascript Advance/Aync-Await.js
+++ Javascript/Practice/Javascript Advance/Aync-Await.js
@@ -1,5 +1,5 @@
 function fetchData() {
-  return new Promise((resolve, reject), () => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve({ name: "chaicode", url: "https://chaicode.com" });
     }, 3000);
@@ -11,6 +11,7 @@ async function getUserData() {
     console.log("Fetching user data...");
     let data = await fetchData();
     console.log("User Data : ", data);
+    console.log("Fetched user data successfully!");
   } catch (error) {
     console.error("Error fetching user data:", error);
   }
